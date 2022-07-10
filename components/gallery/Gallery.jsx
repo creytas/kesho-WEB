9482078c70cb894930a73927c329c0ce15434e85
CR3Y745 <creytas@gmail.com>
2022-07-10 01:57:16 +0100

    feat: create landing page components and stylize them

--- components/gallery/Gallery.jsx
+++ components/gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TextField from "../title/TextField";
+import TextField from "../utils/TextField";
 
 const Gallery = () => {
   return (
@@ -12,6 +12,57 @@ const Gallery = () => {
         title="GALLERY"
         content="This worth a thousand words"
       />
+      <p style={{ margin: "1% auto", fontSize: "20px", width: "82%" }}>
+        Et et est atque enim explicabo facilis. Molestiae nesciunt dolorem
+        deserunt esse fuga. Autem quae et porro non pariatur quidem molestiae.
+        Rem totam aliquid debitis exercitationem minima ipsam nulla quam
+        ducimus.
+      </p>
+      <div
+        style={{
+          border: "0px solid red",
+          display: "flex",
+          flexWrap: "wrap",
+          width: "85%",
+          margin: "3% auto",
+        }}
+      >
+        <img
+          src="/img/image_13.png"
+          alt="gallery"
+          style={{ width: "30%", margin: "1%" }}
+        />
+        <img
+          src="/img/image_24.png"
+          alt="gallery"
+          style={{ width: "30%", margin: "1%" }}
+        />
+        <img
+          src="/img/image_26.png"
+          alt="gallery"
+          style={{ width: "30%", margin: "1%" }}
+        />
+        <img
+          src="/img/image_17.png"
+          alt="gallery"
+          style={{ width: "30%", margin: "1%" }}
+        />
+        <img
+          src="/img/image_20.png"
+          alt="gallery"
+          style={{ width: "30%", margin: "1%" }}
+        />
+        <img
+          src="/img/image_27.png"
+          alt="gallery"
+          style={{
+            width: "30%",
+            height: "238px",
+            objectFit: "cover",
+            margin: "1%",
+          }}
+        />
+      </div>
     </section>
   );
 };
--- components/team/Team.jsx
+++ components/team/Team.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TextField from "../title/TextField";
+import TextField from "../utils/TextField";
 const background = "/img/background.png";
 
 const Team = () => {
